5b61edc617fa679420fe40290b68e3ac83b74100
Frederic Lesage <frederic.lesage@polymtl.ca>
2019-05-03 12:51:45 -0400

    Some implementation.

--- motorclass.cpp
+++ motorclass.cpp
@@ -1,148 +1,194 @@
 #include "motorclass.h"
 #include <iostream>
+#include <QThread>
+
 MotorClass::MotorClass()
 {
-
-
-    port.setPortName("COM9");
-    std::cerr << port.open(QIODevice::ReadWrite) << std::endl;
-    qDebug() << port.errorString();
-
-    port.setBaudRate(QSerialPort::Baud9600);
-    port.setDataBits(QSerialPort::Data8);
-    port.setParity(QSerialPort::NoParity);
-    port.setStopBits(QSerialPort::OneStop);
-    port.setFlowControl(QSerialPort::NoFlowControl);
-
-    port.flush();
-    std::cerr << port.write("1MO\r", 4) << std::endl;
-//        self.ser.open()
-//        self.ser.flushInput()
-//        self.ser.flushOutput()
-//        self.ser.write('1MO\r')
-//        self.ser.write('2MO\r')
-//        self.ser.write('3MO\r')
-//        self.home()
+    is_open = false;
 }
 
 MotorClass::~MotorClass()
 {
-    port.close();
+    ClosePort();
+}
 
+void OpenPort()
+{
+    if(!is_open)
+    {
+        port.setPortName("COM9");
+        port.open(QSerialPort::ReadWrite);
+        port.setBaudRate(921600);
+        port.setDataBits(QSerialPort::Data8);
+        port.setParity(QSerialPort::NoParity);
+        port.setStopBits(QSerialPort::OneStop);
+        port.setFlowControl(QSerialPort::HardwareControl);
+        port.flush();
+        port.write("1MO\r", 4);
+        port.write("2MO\r", 4);
+        port.write("3MO\r", 4);
+        is_open=true;
+    }
+}
+
+void ClosePort()
+{
+    if(is_open)
+    {
+        port.close();
+        is_open=false;
+    }
 }
 
 void MotorClass::Home()
 {
-/*
-  #        Wait until homing is done to return from this function to
-  #        make sure we have accurate positioning
-          #Motor 1
-          print('Homing X Motor')
-          done=0
-          self.ser.write('1OR0\r')
-          #self.ser.write('1PA0\r')
-          while not done:
-                  self.ser.write('1MD?\r')
-                  done = self.ser.read(4);
-                  time.sleep(1)
+    if(is_open)
+    {
+        char buffer[4];
 
-          #self.ser.write('1PA0\r')
-          #Motor 2
-          #=======================================================================
-          print('Homing Y Motor')
-          done=0
-          self.ser.write('2OR0\r')
-          while not done:
-                  self.ser.write('2MD?\r')
-                  done = self.ser.read(4);
-                  time.sleep(1)
-          #=======================================================================
-          #Motor 3
-          #=======================================================================
-          print('Homing Z Motor')
-          done=0
-          self.ser.write('3OR0\r')
-          while not done:
-                  self.ser.write('3MD?\r')
-                  done = self.ser.read(4);
-                  time.sleep(1)
-          #=======================================================================
+        //        Wait until homing is done to return from this function to
+        //        make sure we have accurate positioning
+        //        Motor 1
+        int* done = (int*) &buffer;
+        port.write("1OR0\r",5);
+        do
+        {
+            port.write("1MD?\r",5);
+            port.read(buffer,4);
+            QThread::sleep(0.2);
+        } while(!done);
 
-  #        Center each motor in the center of their stride
+        *done=0;
+        port.write("2OR0\r",5);
+        do
+        {
+            port.write("2MD?\r",5);
+            port.read(buffer,4);
+            QThread::sleep(0.2);
+        } while(!done);
 
-          #Motor 1
-          #self.move_dx(-27)
-          #self.ser.write('1PA12.5\r')
-          #while not done:
-          #        self.ser.write('1MD?\r')
-          #        done = self.ser.read(4);
-          #        time.sleep(1)
-          */
+        *done=0;
+        port.write("3OR0\r",5);
+        do
+        {
+            port.write("3MD?\r",5);
+            port.read(buffer,4);
+            QThread::sleep(0.2);
+        } while(!done);
+    }
 
 }
 
+void MotorClass::move_dx(float dist)
+{
+    if(is_open)
+    {
+        port.flush();
+        QString tmp;
+        tmp.sprintf("1PR%.2f\r",dist);
+        port.write(tmp.toUtf8());
+        char data=0;
+        do
+        {
+            QThread::sleep(0.1);
+            port.write('1MD?\r');
+            port.read(data,1);
+        } while(!data);
+    }
+}
+
+void MotorClass::move_dy(float dist)
+{
+    if(is_open)
+    {
+        port.flush();
+        QString tmp;
+        tmp.sprintf("2PR%.2f\r",dist);
+        port.write(tmp.toUtf8());
+        char data=0;
+        do
+        {
+            QThread::sleep(0.1);
+            port.write('2MD?\r');
+            port.read(data,1);
+        } while(!data);
+    }
+}
+
+void MotorClass::move_dz(float dist)
+{
+    if(is_open)
+    {
+        port.flush();
+        QString tmp;
+        tmp.sprintf("3PR%.2f\r",dist);
+        port.write(tmp.toUtf8());
+        char data=0;
+        do
+        {
+            QThread::sleep(0.1);
+            port.write('3MD?\r');
+            port.read(data,1);
+        } while(!data);
+    }
+}
+
+void MotorClass::move_ax(float dist)
+{
+    if(is_open)
+    {
+        port.flush();
+        QString tmp;
+        tmp.sprintf("1PA%.2f\r",dist);
+        port.write(tmp.toUtf8());
+        char data=0;
+        do
+        {
+            QThread::sleep(0.1);
+            port.write('1MD?\r');
+            port.read(data,1);
+        } while(!data);
+    }
+}
+
+void MotorClass::move_ay(float dist)
+{
+    if(is_open)
+    {
+        port.flush();
+        QString tmp;
+        tmp.sprintf("2PA%.2f\r",dist);
+        port.write(tmp.toUtf8());
+        char data=0;
+        do
+        {
+            QThread::sleep(0.1);
+            port.write('2MD?\r');
+            port.read(data,1);
+        } while(!data);
+    }
+}
+
+void MotorClass::move_az(float dist)
+{
+    if(is_open)
+    {
+        port.flush();
+        QString tmp;
+        tmp.sprintf("3PA%.2f\r",dist);
+        port.write(tmp.toUtf8());
+        char data=0;
+        do
+        {
+            QThread::sleep(0.1);
+            port.write('3MD?\r');
+            port.read(data,1);
+        } while(!data);
+    }
+}
 
 /*
-def move_dx(self,dx):
-    #print(dx)
-    self.ser.flushInput()
-    self.ser.flushOutput()
-    self.ser.write('1PR'+str(dx)+'\r')
-    data=0
-    while not data:
-        time.sleep(0.1)
-        self.ser.write('1MD?\r')
-        data = self.ser.read(1);
 
-def move_dy(self,dy):
-    self.ser.flushInput()
-    self.ser.flushOutput()
-    data=0
-    self.ser.write('2PR'+str(dy)+'\r')
-    while not data:
-        self.ser.write('2MD?\r')
-        data = self.ser.read(1);
-        time.sleep(0.1)
-
-def move_dz(self,dz):
-    self.ser.flushInput()
-    self.ser.flushOutput()
-    done=0
-    self.ser.write('3PR'+str(dz)+'\r')
-    while not done:
-        self.ser.write('3MD?\r')
-        done = self.ser.read(1);
-        time.sleep(0.1)
-
-def move_ax(self,x_pos):
-    self.ser.flushInput()
-    self.ser.flushOutput()
-    data=0
-    self.ser.write('1PA'+str(x_pos)+'\r')
-    while not data:
-        self.ser.write('1MD?\r')
-        data = self.ser.read();
-        time.sleep(0.1)
-
-def move_ay(self,y_pos):
-    self.ser.flushInput()
-    self.ser.flushOutput()
-    done=0
-    self.ser.write('2PA'+str(y_pos)+'\r')
-    while not done:
-        self.ser.write('2MD?\r')
-        done = self.ser.read(1000);
-        time.sleep(0.1)
-
-def move_az(self,z_pos):
-    self.ser.flushInput()
-    self.ser.flushOutput()
-    self.ser.write('3PA'+str(z_pos)+'\r')
-    done=0
-    while not done:
-        self.ser.write('3MD?\r')
-        done = self.ser.read(1000);
-        time.sleep(0.1)
 
 def get_pos_axial(self):
     self.ser.flushInput()
--- motorclass.h
+++ motorclass.h
@@ -7,10 +7,19 @@ class MotorClass
 public:
     MotorClass();
     virtual ~MotorClass();
+    void OpenPort();
+    void ClosePort();
     void Home();
+    void move_dx(float dist);
+    void move_dy(float dist);
+    void move_dz(float dist);
+    void move_ax(float dist);
+    void move_ay(float dist);
+    void move_az(float dist);
 
 private:
     QSerialPort port;
+    bool is_open;
 };
 
 #endif // MOTORCLASS_H
