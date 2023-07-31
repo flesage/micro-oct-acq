b7b9b8ed6a2ad48c7fc59923f37c3fd12799c4b4
Joël Lefebvre <lefebvre.joel@uqam.ca>
2023-07-31 13:45:08 -0400

    Removing the simulation mode

--- config.h
+++ config.h
@@ -28,7 +28,7 @@
 #define ROTATION_ACC2ENC 14.66 // deg/s2, Thorlabs communication protocol, section 8
 #define ROTATION_REFRESH_RATE 200 // ms
 
-#define SIMULATION
+//#define SIMULATION
 
 // OCT Server Configuration
 #define SERVER_SOCKET_PORT 65432
